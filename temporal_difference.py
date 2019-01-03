be02ef65f4edea79995accbaa91819c2cd65cc6a
Aakash Pattabi <15pattabia@gmail.com>
2019-01-03 15:50:29 -0600

    Added subclass for (tabular) Q-learner

--- temporal_difference.py
+++ temporal_difference.py
@@ -98,7 +98,6 @@ defined.
 '''
 class TemporalDifferenceLearner(object):
 	def __init__(self, exp, lr, discount, epsilon, epsilon_decay, predictor):
-
 		self.exp = exp
 		self.learning_rate = lr
 		self.discount_rate = discount
@@ -140,12 +139,11 @@ class TemporalDifferenceLearner(object):
 
 class SARSALearner(TemporalDifferenceLearner):
 	def __init__(self, exp, lr, discount, epsilon, epsilon_decay, predictor):
-
 		TemporalDifferenceLearner.__init__(self, exp, lr, discount, epsilon, epsilon_decay, 
 			predictor)
 
 	def update_parameters(self, s, a, r, sp, ap, env):
-		## See pg. 10 in Geist and Pietquin (2010)
+		## See pg. 10 in Geist and Pietquin (2010), equation #36 
 		self.predictor.zero_grad()
 		s_a = self.predictor.encode_input(a, s, env)
 		Qs_a = self.predictor.forward(s_a)
@@ -155,6 +153,30 @@ class SARSALearner(TemporalDifferenceLearner):
 		Qsp_ap = self.predictor.forward(sp_ap)
 
 		td_error = r + self.discount_rate * (Qsp_ap) - Qs_a
+
+		for param in self.predictor.parameters():
+			if param.requires_grad:
+				param = param + self.learning_rate * param.grad * td_error
+
+class QLearner(TemporalDifferenceLearner):
+	def __init__(self, exp, lr, discount, epsilon, epsilon_decay, predictor):
+		TemporalDifferenceLearner.__init__(self, exp, lr, discount, epsilon, epsilon_decay, 
+			predictor)
+
+	def update_parameters(self, s, a, r, sp, env):
+		## See pg. 10 in Geist and Pietquin (2010), equation #41
+		self.predictor.zero_grad()
+		s_a = self.predictor.encode_input(a, s, env)
+		Qs_a = self.predictor.forward(s_a)
+		Qs_a.backward()
+
+		all_Qs_a = []
+		for action in range(env.action_space.n):
+			s_a = self.predictor.encode_input(action, s, env)
+			current_Qs_a = self.predictor.forward(s_a)
+			all_Qs_a.append(current_Qs_a)
+
+		td_error = r + self.discount_rate * (np.max(all_Qs_a)) - Qs_a
 
 		for param in self.predictor.parameters():
 			if param.requires_grad:
